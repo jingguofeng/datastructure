95da9609a22dacbe2e02ecfdf0b450ba077ca7b5
jingguofeng <everjgfeng@gmail.com>
2015-04-13 14:39:15 -0500

    add tree structures

---
+++ datastructure/src/treeStructures/BinaryTree.java
@@ -0,0 +1,7 @@
+package treeStructures;
+
+/**
+ * Created by jingguo on 4/13/2015.
+ */
+public class BinaryTree {
+}
---
+++ datastructure/src/treeStructures/Position.java
@@ -0,0 +1,8 @@
+package treeStructures;
+
+/**
+ * Created by jingguo on 4/13/2015.
+ */
+public interface Position<E> {
+    E getElement() throws IllegalStateException;
+}
